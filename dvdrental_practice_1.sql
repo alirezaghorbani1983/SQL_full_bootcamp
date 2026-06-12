2dce9fe611d50efc482c3da1269b7788dd12bff5
Ali <alirezaghorbani.bmedu@yahoo.com>
2026-06-12 23:46:11 +0200

    LEFT JOIN with WHERE

--- dvdrental_practice_1.sql
+++ dvdrental_practice_1.sql
@@ -226,4 +226,5 @@ ON inventory.film_id = film.film_id;
 
 SELECT film.film_id, title, inventory_id, store_id FROM film
 LEFT JOIN inventory
-ON inventory.film_id = film.film_id;
+ON inventory.film_id = film.film_id
+WHERE inventory.film_id IS NULL;
