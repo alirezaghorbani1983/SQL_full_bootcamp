0cf1bf6ebdf7b68028f0cb18556aa179982fbe0f
Ali <alirezaghorbani.bmedu@yahoo.com>
2026-06-26 18:58:27 +0200

    new begin

--- dvdrental_practice_1.sql
+++ dvdrental_practice_1.sql
@@ -222,9 +222,21 @@ SELECT * FROM inventory;
 SELECT film.film_id, title, inventory_id FROM film
 LEFT JOIN inventory
 ON inventory.film_id = film.film_id;
+
+SELECT film.film_id, title, inventory_id, store_id FROM film
+LEFT JOIN inventory
+ON inventory.film_id = film.film_id;
  
+SELECT film.film_id, title, inventory_id, store_id FROM film
+LEFT JOIN inventory
+ON inventory.film_id = film.film_id
+WHERE inventory.film_id IS NULL;
+
 
 SELECT film.film_id, title, inventory_id, store_id FROM film
 LEFT JOIN inventory
 ON inventory.film_id = film.film_id
 WHERE inventory.film_id IS NULL;
+
+
+SELECT * FROM customer;
